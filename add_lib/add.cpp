b8d77a2fb96b61486e513cd1f584805894bf5e86
Tuhin Paul <Tuhin.Paul@in.bosch.com>
2024-09-08 11:05:34 +0530

    adding logging

--- add_lib/add.cpp
+++ add_lib/add.cpp
@@ -1,5 +1,7 @@
 #include "add.h"
+#include <iostream>
 
 int add(int a, int b) {
+    std::cout << "LOGGING [add.cpp]: Adding " << a << " and " << b << std::endl;  // Logging the addition operation
     return a + b;
 }
--- mul_lib/mul.cpp
+++ mul_lib/mul.cpp
@@ -1,5 +1,7 @@
 #include "mul.h"
+#include <iostream>
 
 int multiply(int a, int b) {
+    std::cout << "LOGGING [mul.cpp]: Multiplying " << a << " and " << b << std::endl;  // Logging the multiplication operation
     return a * b;
 }
